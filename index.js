a33d956381736606fb743203e316936fc5ae47f0
Mary Watiri <watirimary30@gmail.com>
2023-12-18 08:26:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Akin"];
+function writeCards(names) {
+  let thankYouMessages = [];
+  for (let m = 0; m < names.length; m++) {
+  let message = `Thank you, ${names[m]}, for the wonderful surprise gift!`;
+
+  thankYouMessages.push(message);
+  }
+ return thankYouMessages;
+}
+console.log(writeCards(names));
+function countDown(number){
+  let  z = 10;
+  while(z>=0){
+    console.log(z);
+    z--;
+  }
+}
+countDown(10);
+
+    
+
+
+    
+
+
